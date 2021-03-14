747e171fe57d642f84ed28fcabdc2c8ee44c7097
Anukritik007 <anukritik77@gmail.com>
2021-03-14 13:41:15 +0530

    showAdd button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,14 +2,14 @@ import PropTypes from "prop-types";
 import Button from "./Button";
 
 const Header = (props) => {
-  const onClickHandler = () => {
-    console.log("Clicked!");
-  };
-
   return (
     <header className="header">
       <h1>{props.title}</h1>
-      <Button color="green" text="Add" onClick={onClickHandler} />
+      <Button
+        color={props.showAdd ? "red" : "green"}
+        text={props.showAdd ? "Close" : "Add"}
+        onClick={props.onAddClick}
+      />
     </header>
   );
 };
